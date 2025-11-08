e226627a2fe2e0bbce64b76f32c5ccfc1b717f15
edson <edsonlimacode@gmail.com>
2025-11-07 22:06:06 -0300

    ajustes

--- src/main/java/com/springfood/api/controller/ProductController.java
+++ src/main/java/com/springfood/api/controller/ProductController.java
@@ -8,13 +8,12 @@ import com.springfood.api.openapi.controller.ProductControllerDoc;
 import com.springfood.core.security.CheckSecurity;
 import com.springfood.domain.model.Product;
 import com.springfood.domain.service.ProductService;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
